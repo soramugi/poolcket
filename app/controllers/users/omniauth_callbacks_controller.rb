d89fb83e367102e00b099dc06f7ce60ec3d0bcfb
soramugi <soramugi.chika@gmail.com>
2014-04-06 21:42:14 +0900

    pocket ログイン

---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,18 @@
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  def pocket
+    @user = User.find_or_create_by(user_params)
+
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication
+      set_flash_message( :notice, :success, kind: 'Pocket' ) if is_navigational_format?
+    else
+      session['devise.pocket_data'] = request.env['omniauth.auth']
+      redirect_to new_user_registration_url
+    end
+  end
+
+  private
+  def user_params
+    request.env['omniauth.auth'].slice(:provider, :uid).to_h
+  end
+end
