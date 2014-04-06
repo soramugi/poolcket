33cc0ee5ef6f8e4e869a0afd10b51982d7df13e5
soramugi <soramugi.chika@gmail.com>
2014-04-06 20:54:10 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Poolcket::Application.config.secret_key_base = '75e86c7ceaceb813c318b6ebcf9a2158776552fab285cd39355732f8f5d392577223468d914f12a4bfc65de2f1d9f54cfdf9ac8a6578af2446c52739d12f231d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Poolcket::Application.config.session_store :cookie_store, key: '_poolcket_session'
