fca71519839ed60a4211d0243c54e6d7950136fb
soramugi <soramugi.chika@gmail.com>
2014-04-07 00:57:16 +0900

    add settings.yml

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,7 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+ENV.update YAML.load_file('config/settings.yml')[Rails.env] rescue {}
 
 module Poolcket
   class Application < Rails::Application
