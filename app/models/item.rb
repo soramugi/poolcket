a4b10e6a45ce71762187a9bba0f297957dd248e1
soramugi <soramugi.chika@gmail.com>
2014-04-07 10:29:54 +0900

    ワンライナー

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,9 +2,7 @@ class Item < ActiveRecord::Base
   belongs_to :user
 
   def self.all_create user_id,arr
-    arr.map do |i,h|
-      create h.merge({user_id: user_id})
-    end
+    arr.map { |i,h| create h.merge({user_id: user_id}) }
   end
 
   before_create do |c|
