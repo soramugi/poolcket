c8006f594596c26dc7f821fd7b7024d9b9291d6c
soramugi <soramugi.chika@gmail.com>
2014-04-07 10:53:53 +0900

    外部プレイヤーの表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,23 @@
 class Item < ActiveRecord::Base
   belongs_to :user
 
+  def nicovideo?
+    given_url.include?('nicovideo.jp/watch/')
+  end
+
+  def nico_id
+    given_url.match(/(sm[0-9]+)|(nm[0-9]+)/).captures.compact.first
+  end
+
+  def external_player
+    "<script type='text/javascript' src='http://ext.nicovideo.jp/thumb_watch/#{nico_id}'></script><noscript><a href='http://www.nicovideo.jp/watch/#{nico_id}'>#{given_title}</a></noscript>" if nicovideo?
+  end
+
   def self.all_create user_id,arr
     arr.map { |i,h| create h.merge({user_id: user_id}) }
   end
 
   before_create do |c|
-    c.given_url.include?('nicovideo.jp/watch/')
+    c.nicovideo?
   end
 end
