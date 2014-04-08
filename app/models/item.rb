273474d761275fe0b1c7f990f6e3de735a535085
soramugi <soramugi.chika@gmail.com>
2014-04-08 13:16:27 +0900

    連続再生

--- app/models/item.rb
+++ app/models/item.rb
@@ -23,6 +23,11 @@ class Item < ActiveRecord::Base
     end
   end
 
+  def next
+    _item = Item.where("id > #{id} and user_id = #{user_id}").limit(1).first
+    _item.nil? ?  Item.where(user_id: user_id).limit(1).first : _item
+  end
+
   def self.all_create user_id,arr
     arr.map { |i,h| create h.merge({user_id: user_id}) }
   end
