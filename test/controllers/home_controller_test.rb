d476eb26860f0e7c5b5bb8e84b8e28527ab0f9dc
soramugi <soramugi.chika@gmail.com>
2014-04-08 13:53:34 +0900

    controllersテスト

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,9 +1,20 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
-  test "should get index" do
+  include Devise::TestHelpers
+
+  test "should not login get index" do
     get :index
     assert_response :success
+    assert_equal [], assigns['items']
+  end
+
+  test "should login get index" do
+    user = users(:one)
+    sign_in user
+    get :index
+    assert_response :success
+    assert_equal user.items, assigns['items']
   end
 
 end
