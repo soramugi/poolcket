e53dc547f0e7d28fd57f88a54604fe967becc55f
soramugi <soramugi.chika@gmail.com>
2014-07-01 14:05:32 +0900

    表示をそれっぽく

--- config/application.rb
+++ config/application.rb
@@ -9,17 +9,10 @@ ENV.update YAML.load_file('config/settings.yml')[Rails.env] rescue {}
 
 module Poolcket
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
 
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
     config.consumer_key = ENV['CONSUMER_KEY'] || 'YOUR_CONSUMER_KEY'
+    config.assets.paths << "#{config.root}/vender/assets/fonts"
   end
 end
